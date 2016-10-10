ffc44e832a75b58e917f197af1f0c57099ce9124
JosephBuchwald <buchwaldjl01@uww.edu>
2016-10-09 21:00:05 -0500

    Create Hero.java

---
+++ zombiewar_java/zombiewar_java/src/zombiewar/impl/Hero.java
@@ -0,0 +1,18 @@
+package zombiewar.impl;
+
+import zombiewar.intf.ISurvivor;
+import zombiewar.intf.IZombie;
+
+public class Hero extends Character implements ISurvivor{
+	
+	private static int iterator = 0;
+	Stalker(int health){
+		super(health,"Hero "+iterator);
+		iterator++;
+	}
+	
+	public void attack(IZombie zombie){
+		zombie.decreaseHealth(20);
+	}
+	
+}
